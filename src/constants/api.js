bffd5f168bb85f25e7123a0ca2907169f1e9f7b8
pySoo <soopy368@gmail.com>
2023-10-27 16:49:58 +0900

    [feat] : 사용하지 않는 코드 제거

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,9 +1,5 @@
 export const BASE_URL = "https://kdt-frontend.programmers.co.kr";
 
-export const END_POINT = {
-  searchPerformance: "/api/search?keyword=",
-};
-
 export const HTTP_METHOD = {
   GET: "GET",
   POST: "POST",
